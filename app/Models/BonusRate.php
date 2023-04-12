c93607e3d66884048490873011ec2ad6ffc9d0b7
kirill-kilyakov <kluknulo@mail.ru>
2023-04-12 15:48:34 +0300

    alpha version

--- app/Models/BonusRate.php
+++ app/Models/BonusRate.php
@@ -14,6 +14,6 @@ class BonusRate extends Model
 
     public function users()
     {
-        return $this->hasMany(User::class, 'bank_rate_id', 'id');
+        return $this->hasMany(User::class, 'bonus_rate_id', 'id');
     }
 }
